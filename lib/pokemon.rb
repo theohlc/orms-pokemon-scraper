4c3106c35bfa1314684c956c28f8619002f1533e
theo <teddyhoover@gmail.com>
2021-01-16 12:00:12 -0600

    commit tree 9adca65fd6d1a6eceb4ccbc671042a24928a7f9c

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,27 @@
 class Pokemon
+    attr_accessor :id, :name, :type, :db
+
+    def initialize(row)
+        @id = row[:id]
+        @name = row[:name]
+        @type = row[:type]
+        @db = row[:db]
+    end
+
+    def self.save(name, type, db)
+        db.execute('INSERT INTO pokemon (name, type) VALUES (?,?)', name, type)
+    end
+
+    def self.find(id, db)
+        row = db.execute('SELECT name, type FROM pokemon WHERE id = ?', id)[0]
+        row_hash = {
+            :id => id,
+            :name => row[0],
+            :type => row[1],
+            :db => db
+        }
+        
+        new(row_hash)
+    end
+
 end
